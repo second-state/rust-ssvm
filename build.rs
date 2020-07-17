e6026c222cab6fb16f47fc93dd50763a658b050e
vincent <vincent@secondstate.io>
2020-07-17 09:39:44 +0800

    [SSVM][EVMC] Update submodule ssvm and evmc version.
    1. Use newest ssvm support evmc codebase.
    2. Upgrade evmc abi version from 6.x to 7.x.

--- build.rs
+++ build.rs
@@ -19,10 +19,10 @@ extern crate cmake;
 use cmake::Config;
 
 fn build_link_ssvm_dylib() {
-    let dst = Config::new("SSVM").no_build_target(true).build();
+    let dst = Config::new("ssvm-evmc").no_build_target(true).build();
     let evmcssvm_path = Path::new(&dst).join("build/tools/ssvm-evmc");
     println!("cargo:rustc-link-search=native={}", evmcssvm_path.display());
-    println!("cargo:rustc-link-lib=dylib=ssvmEVMC");
+    println!("cargo:rustc-link-lib=dylib=ssvm-evmc");
 }
 
 fn main() {
